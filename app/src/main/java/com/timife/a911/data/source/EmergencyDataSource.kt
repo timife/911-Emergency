c23dbddd1ca1c919fd476e507883de4d374af236
update <timife007@gmail.com>
2021-06-24 15:56:01 -0400

    datasource interface

--- app/src/main/java/com/timife/a911/data/source/EmergencyDataSource.kt
+++ app/src/main/java/com/timife/a911/data/source/EmergencyDataSource.kt
@@ -1,2 +1,16 @@
 package com.timife.a911.data.source
 
+import androidx.lifecycle.LiveData
+import com.timife.a911.data.EmergencyInfo
+import com.timife.a911.data.Result
+
+interface EmergencyDataSource {
+    suspend fun upsert(item: EmergencyInfo)
+
+    suspend fun delete(item: EmergencyInfo)
+
+    suspend fun checkIfExist(item: EmergencyInfo): Result<Boolean>
+
+    suspend fun getEmergencyServicesItem() : Result<List<EmergencyInfo>>
+    suspend fun getNonEmergencyServicesItem(): LiveData<List<EmergencyInfo>>
+}
--- app/src/main/java/com/timife/a911/data/source/local/EmergencyLocalDataSource.kt
+++ app/src/main/java/com/timife/a911/data/source/local/EmergencyLocalDataSource.kt
@@ -1,2 +1,35 @@
 package com.timife.a911.data.source.local
 
+import androidx.lifecycle.LiveData
+import com.timife.a911.data.EmergencyInfo
+import com.timife.a911.data.Result
+import com.timife.a911.data.source.EmergencyDataSource
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+class EmergencyLocalDataSource(
+        private val emergencyDao: EmergencyDao,
+        private val ioDispatcher: CoroutineDispatcher =
+                Dispatchers.IO
+) :EmergencyDataSource{
+    override suspend fun upsert(item: EmergencyInfo) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun delete(item: EmergencyInfo) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun checkIfExist(item: EmergencyInfo): Result<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getEmergencyServicesItem(): Result<List<EmergencyInfo>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getNonEmergencyServicesItem(): LiveData<List<EmergencyInfo>> {
+        TODO("Not yet implemented")
+    }
+
+}
