f926d5a318e4e79158c9815f010b3e09e5fb2623
update <timife007@gmail.com>
2021-07-24 08:08:10 -0400

    theming

--- app/src/main/java/com/timife/a911/emergencyPref/di/PrefModule.kt
+++ app/src/main/java/com/timife/a911/emergencyPref/di/PrefModule.kt
@@ -4,7 +4,6 @@ package com.timife.a911.emergencyPref.di
 import androidx.lifecycle.ViewModel
 import com.timife.a911.di.modules.ViewModelKey
 import com.timife.a911.emergencyPref.ui.PrefViewModel
-import com.timife.a911.emergencySave.ui.SaveViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
