88125d46bc075a13f9d2bd19e9cd9b669199e2f2
update <timife007@gmail.com>
2021-07-25 06:18:11 -0400

    font an manifest permission

--- app/src/main/java/com/timife/a911/emergencyPref/di/PrefModule.kt
+++ app/src/main/java/com/timife/a911/emergencyPref/di/PrefModule.kt
@@ -1,6 +1,5 @@
 package com.timife.a911.emergencyPref.di
 
-//import com.timife.a911.di.modules.ViewModelKey
 import androidx.lifecycle.ViewModel
 import com.timife.a911.di.modules.ViewModelKey
 import com.timife.a911.emergencyPref.ui.PrefViewModel
@@ -8,7 +7,6 @@ import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 
-////
 @Module
 abstract class PrefModule {
 
