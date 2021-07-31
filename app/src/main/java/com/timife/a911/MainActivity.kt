d2914a6ab418a626e6308711b2d68467e04dcc54
update <timife007@gmail.com>
2021-07-31 06:35:28 -0400

    filter menu

--- app/src/main/java/com/timife/a911/MainActivity.kt
+++ app/src/main/java/com/timife/a911/MainActivity.kt
@@ -2,6 +2,7 @@ package com.timife.a911
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         (applicationContext as EmergencyApplication).emergencyComponent.inject(this)
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val navHostFragment =
--- app/src/main/java/com/timife/a911/emergencySave/ui/SaveFragment.kt
+++ app/src/main/java/com/timife/a911/emergencySave/ui/SaveFragment.kt
@@ -2,15 +2,15 @@ package com.timife.a911.emergencySave.ui
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.ravikoradiya.library.CenterTitle
 import com.timife.a911.EmergencyApplication
+import com.timife.a911.R
 import com.timife.a911.databinding.FragmentSaveBinding
 
 class SaveFragment : Fragment() {
@@ -29,9 +29,13 @@ class SaveFragment : Fragment() {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         (requireActivity().application as EmergencyApplication).emergencyComponent.inject(this)
-
         navController = findNavController()
         binding.saveToolbar.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.save_menu,menu)
     }
 
 }
