809ff30b980dcf47ee2f4d52f543cd9f39c63c53
update <timife007@gmail.com>
2021-12-22 20:37:05 -0500

    onboarding adapter

--- app/src/main/java/com/timife/a911/ui/onBoarding/OnBoardingAdapter.kt
+++ app/src/main/java/com/timife/a911/ui/onBoarding/OnBoardingAdapter.kt
@@ -1,7 +1,39 @@
 package com.timife.a911.ui.onBoarding
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.timife.a911.data.OnBoardingSlideItem
+import com.timife.a911.databinding.SlideItemContainerBinding
+
 /**
  * Created by Timothy Ademola on 22-12-2021
  */
-class OnBoardingAdapter {
+class OnBoardingAdapter (private val sliderItems: List<OnBoardingSlideItem>) :
+    RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
+
+
+    class OnBoardingViewHolder(private val binding: SlideItemContainerBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(introSlideItem: OnBoardingSlideItem) {
+            binding.sliderTitle.text = introSlideItem.title
+            binding.sliderTextDescription.text = introSlideItem.description
+            binding.sliderImage.setImageResource(introSlideItem.icon)
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder {
+        val binding = SlideItemContainerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return OnBoardingViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return sliderItems.size
+    }
+
+    override fun onBindViewHolder(holder: OnBoardingViewHolder, position: Int) {
+        holder.bind(sliderItems[position])
+    }
 }
--- app/src/main/java/com/timife/a911/ui/onBoarding/OnBoardingFragment.kt
+++ app/src/main/java/com/timife/a911/ui/onBoarding/OnBoardingFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import androidx.viewpager2.widget.ViewPager2
 import com.timife.a911.R
+import com.timife.a911.data.OnBoardingSlideItem
+import com.timife.a911.databinding.FragmentOnBoardingBinding
+import com.timife.a911.extensions.navigateSafe
+import com.timife.a911.utils.DepthPageTransformer
 
 /**
  * Created by Timothy Ademola on 22-12-21
  */
 class OnBoardingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+ private lateinit var binding : FragmentOnBoardingBinding
+    private lateinit var adapter: OnBoardingAdapter
 
 
     override fun onCreateView(
@@ -21,7 +29,103 @@ class OnBoardingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_boarding, container, false)
+        binding = FragmentOnBoardingBinding.inflate(inflater)
+        adapter = OnBoardingAdapter(onBoardingItems())
+        binding.onboardSliderViewPager.adapter = adapter
+        binding.onboardSliderViewPager.setPageTransformer(DepthPageTransformer())
+        setupIndicators()
+        setCurrentIndicator(index = 0)
+        binding.onboardSliderViewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicator(index = position)
+            }
+        })
+
+        binding.next.setOnClickListener {
+            if (binding.onboardSliderViewPager.currentItem + 1 < adapter.itemCount) {
+                binding.onboardSliderViewPager.currentItem += 1
+            } else {
+                navigateToLogin()
+            }
+        }
+
+        binding.skipOnboarding.setOnClickListener {
+            navigateToLogin()
+        }
+        return binding.root
+    }
+
+    private fun navigateToLogin() {
+        navigateSafe(OnBoardingFragmentDirections
+            .actionOnBoardingFragmentToAuthNavigation())
+    }
+
+    private fun onBoardingItems(): List<OnBoardingSlideItem> {
+        return listOf(
+            OnBoardingSlideItem(
+                getString(R.string.onboardingTitle1),
+                getString(R.string.onboardingSubtitle1),
+                R.drawable.onboarding_call
+            ),
+            OnBoardingSlideItem(
+                getString(R.string.onboardingTitle2),
+                getString(R.string.onboardingSubtitle2),
+                R.drawable.onboard_search
+            ),
+            OnBoardingSlideItem(
+                getString(R.string.onboardingTitle3),
+                getString(R.string.onboardingSubtitle3),
+                R.drawable.onboard_save
+            )
+        )
+    }
+
+
+    private fun setupIndicators() {
+        val indicators = arrayOfNulls<ImageView>(adapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.setMargins(8, 0, 8, 0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(requireContext())
+            indicators[i].apply {
+                this?.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.onboarding_indicator_inactive
+                    )
+                )
+
+                this?.layoutParams = layoutParams
+            }
+            binding.indicatorContainer.addView(indicators[i])
+        }
+    }
+
+    private fun setCurrentIndicator(index: Int) {
+        val childCount = binding.indicatorContainer.childCount
+        for (i in 0 until childCount) {
+            val imageView = binding.indicatorContainer[i] as ImageView
+            if (i == index) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.onboarding_indicator_active
+                    )
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.onboarding_indicator_inactive
+                    )
+                )
+            }
+
+        }
     }
 
 }
